a0b138bba634a0011d2f9278d5b44bb2ca669e09
Yemile Chávez Martínez <yemi@MacBook-Pro-de-Yemile.local>
2017-11-12 22:06:17 +0100

    Carpetas
    Al crear las carpetas de las Prov con nombres */* se producía un error.
    Algunos nombres de CA y Orov estaban mal

--- CAyP.sh
+++ CAyP.sh
@@ -1,7 +1,7 @@
 #curl http://www.ine.es/daco/daco42/codmun/cod_ccaa_provincia.htm>CAyP.html
 #textutil -convert txt CAyP.html
 #rm CAyP.html
-for ComAuto in "Cataluña" "Andalucía" "Aragón" "Asturias, Principado de" "Balears, Illes" "Canarias" "Cantabria" "Castilla y León" "Castilla"  "Comunitat Valenciana" "Extremadura"  "Galicia" "Madrid, Comunidad de" "Murcia, Región de" "Navarra, Comunidad Foral de" "País Vasco" "Rioja, La" "Ceuta" "Melilla"
+for ComAuto in "Cataluña" "Andalucía" "Aragón" "Asturias, Principado de" "Balears, Illes" "Canarias" "Cantabria" "Castilla y León" "Castilla-La Mancha"  "Comunitat Valenciana" "Extremadura"  "Galicia" "Madrid, Comunidad de" "Murcia, Región de" "Navarra, Comunidad Foral de" "País Vasco" "Rioja, La" "Ceuta" "Melilla"
 	do
 		lineaC=$(grep -w -n -e "^$ComAuto" CAyP.txt|head -n 1|cut -d : -f 1)
 		CodCom=$(head -n $lineaC CAyP.txt|tail -n 2|head -n 1)
@@ -10,12 +10,12 @@ for ComAuto in "Cataluña" "Andalucía" "Aragón" "Asturias, Principado de" "Bal
 	done
 
 
-for Prov in "Almería" "Cádiz" "Córdoba" "Granada" "Huelva" "Jaén" "Málaga" "Sevilla" "Huesca" "Teruel" "Zaragoza" "Asturias" "Balears, Illes" "Palmas, Las" "Santa Cruz de Tenerife" "Cantabria" "Ávila" "Burgos" "León" "Palencia" "Salamanca" "Segovia" "Soria" "Valladolid" "Zamora" "Albacete"  "Ciudad Real" "Cuenca" "Guadalajara" "Toledo" "Barcelona" "Girona" "Lleida" "Tarragona" "Alicante" "Castellón" "Valencia" "Badajoz" "Cáceres" "Coruña, A" "Lugo" "Ourense" "Pontevedra" "Madrid" "Navarra" "Murcia" "Araba" "Bizkaia" "Gipuzkoa"  "Rioja, La"  "Ceuta" "Melilla"
+for Prov in "Almería" "Cádiz" "Córdoba" "Granada" "Huelva" "Jaén" "Málaga" "Sevilla" "Huesca" "Teruel" "Zaragoza" "Asturias" "Balears, Illes" "Palmas, Las" "Santa Cruz de Tenerife" "Cantabria" "Ávila" "Burgos" "León" "Palencia" "Salamanca" "Segovia" "Soria" "Valladolid" "Zamora" "Albacete"  "Ciudad Real" "Cuenca" "Guadalajara" "Toledo" "Barcelona" "Girona" "Lleida" "Tarragona" "Alicante/Alacant" "Castellón/Castelló" "Valencia/València" "Badajoz" "Cáceres" "Coruña, A" "Lugo" "Ourense" "Pontevedra" "Madrid" "Navarra" "Murcia" "Araba/Álava" "Bizkaia" "Gipuzkoa"  "Rioja, La"  "Ceuta" "Melilla"
 	do	
-		lineaP=$(grep -w -n "$Prov" CAyP.txt|tail -n 1|cut -d : -f 1)
+		lineaP=$(grep -w -n -e "^$Prov" CAyP.txt|tail -n 1|cut -d : -f 1)
 		CodCom2=$(head -n $lineaP CAyP.txt|tail -n 4|head -n 1)
 		NomCom2=$(head -n $lineaP CAyP.txt|tail -n 3|head -n 1)
 		CodProv=$(head -n $lineaP CAyP.txt|tail -n 2|head -n 1)
-		NomProv=$(head -n $lineaP CAyP.txt|tail -n 1)
+		NomProv=$(head -n $lineaP CAyP.txt|tail -n 1|cut -d/ -f1)
 		mkdir "$CodCom2"_"$NomCom2"/"$CodProv"_"$NomProv"
 	done 
