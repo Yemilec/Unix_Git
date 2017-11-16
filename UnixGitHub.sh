831a5b8e8cf94ed2df03873ae993692616000917
Yemile Chávez Martínez <yemi@MacBook-Pro-de-Yemile.local>
2017-11-16 17:24:36 +0100

    /dev/null
    Ocultando las salidas del script en la terminal

--- UnixGitHub.sh
+++ UnixGitHub.sh
@@ -4,9 +4,9 @@ for year in {2006..2015}
 		for month in Enero Febrero Marzo Abril  Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre
 			do
 				mkdir "$year"/$month
-				curl -s "http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/${year}/{$month}{%20,_}{$year}.zip" >"$year"/"$month"/"$month"_"$year".zip 
+				curl -s "http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/${year}/{$month}{%20,_}{$year}.zip" >"$year"/"$month"/"$month"_"$year".zip 2> /dev/null 
 				
-				unzip -njq  "$year"/"$month"/"$month"_"$year".zip '*127P*' -d "$year"
+				unzip -njq  "$year"/"$month"/"$month"_"$year".zip '*127P*' -d "$year" 2> /dev/null
 
 				rm -r "$year"/"$month"
 				
