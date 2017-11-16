6e9121122abcbac8931cf009045eb0c90289926e
Yemile Chávez Martínez <yemi@MacBook-Pro-de-Yemile.local>
2017-11-16 17:03:35 +0100

    Carpetas de meses
    Limpieza de las carpetas de años y meses. En las carpetas de años quedan unicamente  los archivos *T_127P*

--- UnixGitHub.sh
+++ UnixGitHub.sh
@@ -5,13 +5,11 @@ for year in {2006..2015}
 			do
 				mkdir "$year"/$month
 				curl "http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/${year}/{$month}{%20,_}{$year}.zip" >"$year"/"$month"/"$month"_"$year".zip 
-				     #http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/2009/Diciembre_2009.zip
-				     #http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/2009/Agosto%202009.zip
-				     #http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/2006/Enero_2006.zip
-				unzip -nj  "$year"/"$month"/"$month"_"$year".zip '*127P*' -d "$year"/"$month"
+				
+				unzip -nj  "$year"/"$month"/"$month"_"$year".zip '*127P*' -d "$year"
 
-				#rm "$year"/"$month"/"$month"_"$year".zip
-				#rm $(ls "$year"/"$month"/*.txt|grep -v T_127P)
+				rm -r "$year"/"$month"
+				
 				
 
 			done
