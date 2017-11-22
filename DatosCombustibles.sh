1ae76a184848ef8f4abb9bcd72cac5e6871b550f
Yemile Chávez Martínez <yemi@MacBook-Pro-de-Yemile.local>
2017-11-22 02:16:19 +0100

    coruña
    error de busqueda de la palabra coruña

--- DatosCombustibles.sh
+++ DatosCombustibles.sh
@@ -2,7 +2,7 @@ for year in 06 #07 08 09 10 11 12 13 14 15
 	do 
 		for month in 01 #02 03 04 05 06 07 08 09 10 11 12
 			do
-				for Prov in  "Almeri*a" "C*diz" "C*rdoba" "Granada" "Huelva" "Jae*n" "M*laga" "Sevilla" "Huesca" "Teruel" "Zaragoza" "Asturias" "Balear" "Palmas" "Tenerife" "Cantabria" "A*vila" "Burgos" "Leo*n " "Palencia" "Salamanca" "Segovia" "Soria" "Valladolid" "Zamora" "Albacete"  "Real" "Cuenca" "Guadalajara" "Toledo" "Barcelona" "Girona" "Lleida" "Tarragona" "Alicante" "Castello*n" "Valencia" "Badajoz" "C*ceres" "Coruña" "Lugo" "O*rense" "Pontevedra" "Madrid" "Navarra" "Murcia" "A[rl]a[bv]a" "[VB]iz[ck]a[iy]a" "G*ipuz*"  "Rioja"  "Ceuta" "Melilla"
+				for Prov in  "Almeri*a" "C*diz" "C*rdoba" "Granada" "Huelva" "Jae*n" "M*laga" "Sevilla" "Huesca" "Teruel" "Zaragoza" "Asturias" "Balear" "Palmas" "Tenerife" "Cantabria" "A*vila" "Burgos" "Leó*n" "Palencia" "Salamanca" "Segovia" "Soria" "Valladolid" "Zamora" "Albacete"  "Real" "Cuenca" "Guadalajara" "Toledo" "Barcelona" "Girona" "Lleida" "Tarragona" "Alicante" "Castello*n" "Valencia" "Badajoz" "C*ceres" "Coruña" "Lugo" "O*rense" "Pontevedra" "Madrid" "Navarra" "Murcia" "A[rl]a[bv]a" "[VB]iz[ck]a[iy]a" "G*ipuz*"  "Rioja"  "Ceuta" "Melilla"
 				
 					do 
 						echo "$Prov" "$year" "$month"
