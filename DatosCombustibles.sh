9a0f86a0ee532a2a19afaf791366590541a97a6f
Yemile Chávez Martínez <yemi@MacBook-Pro-de-Yemile.local>
2017-11-22 17:20:39 +0100

    datos combustibles
    seleccion de los datos de los combustibles deseados, crear un fichero con el nombre des tal combustible

--- DatosCombustibles.sh
+++ DatosCombustibles.sh
@@ -2,17 +2,30 @@ for year in 06 #07 08 09 10 11 12 13 14 15
 	do 
 		for month in 01 #02 03 04 05 06 07 08 09 10 11 12
 			do
-				for Prov in  "Almeri*a" "C*diz" "C*rdoba" "Granada" "Huelva" "Jae*n" "M*laga" "Sevilla" "Huesca" "Teruel" "Zaragoza" "Asturias" "Balear" "Palmas" "Tenerife" "Cantabria" "A*vila" "Burgos" "Leó*n" "Palencia" "Salamanca" "Segovia" "Soria" "Valladolid" "Zamora" "Albacete"  "Real" "Cuenca" "Guadalajara" "Toledo" "Barcelona" "Girona" "Lleida" "Tarragona" "Alicante" "Castello*n" "Valencia" "Badajoz" "C*ceres" "Coruña" "Lugo" "O*rense" "Pontevedra" "Madrid" "Navarra" "Murcia" "A[rl]a[bv]a" "[VB]iz[ck]a[iy]a" "G*ipuz*"  "Rioja"  "Ceuta" "Melilla"
+				for Prov in  "Almeri*a" #"Ca*diz" "Co*rdoba" "Granada" "Huelva" "Jae*n" "Ma*laga" "Sevilla" "Huesca" "Teruel" "Zaragoza" "Asturias" "Balear" "Palmas" "Tenerife" "Cantabria" "A*vila" "Burgos" "Leó*n" "Palencia" "Salamanca" "Segovia" "Soria" "Valladolid" "Zamora" "Albacete"  "Real" "Cuenca" "Guadalajara" "Toledo" "Barcelona" "Girona" "Lleida" "Tarragona" "Alicante" "Castello*n" "Valencia" "Badajoz" "Ca*ceres" "Coruñ*a" #"Lugo" "O*rense" "Pontevedra" "Madrid" "Navarra" "Murcia" "A[rl]a[bv]a" "[VB]iz[ck]a[iy]a" "G*ipuz*"  "Rioja"  "Ceuta" "Melilla"
 				
 					do 
 						echo "$Prov" "$year" "$month"
-						grep -i   "$Prov"  20"$year"/T_127P_$month*.txt
+						Linea=$(grep -i   "$Prov"  20"$year"/T_127P_$month*.txt)
+						
+						grep -i   "$Prov"  20"$year"/T_127P_$month*.txt| awk -F'  '+ '{print $2}'|awk -F. '{print $1$2$3}' | sed "1 i\   
+						20$year$month   " >> COMBUSTIBLENUCLEAR.txt
+
+						# $Linea| awk -F'  '+ '{print $3 > "CARBONES.txt"}'
+						# $Linea| awk -F'  '+ '{print $4 > "LIGNITOS.txt"}'
+						# $Linea| awk -F'  '+ '{print $5 > "FUEL.txt"}'
+						# $Linea| awk -F'  '+ '{print $6 > "GASNATURAL.txt"}'
+						# $Linea| awk -F'  '+ '{print $7 > "OTROSCOMBUSTIBLES.txt"}'
+						# $Linea| awk -F'  '+ '{print $8 > "TOTAL.txt"}'
+
+
+
 						
 
 						
 
 
-						find . -type d|grep -w */*_*$Prov*
+						#find . -type d|grep -i -w */*_*$Prov*
 
 					done 
 				
