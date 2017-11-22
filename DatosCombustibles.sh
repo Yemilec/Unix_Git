6b2518ea81756222ffac14d29207e776ad3aa509
Yemile Chávez Martínez <yemi@MacBook-Pro-de-Yemile.local>
2017-11-22 19:45:44 +0100

    combustibles en todas las provincias

--- DatosCombustibles.sh
+++ DatosCombustibles.sh
@@ -1,31 +1,34 @@
-for year in 06 #07 08 09 10 11 12 13 14 15
+for year in 06 07 08 09 10 11 12 13 14 15
 	do 
-		for month in 01 #02 03 04 05 06 07 08 09 10 11 12
+		for month in 01 02 03 04 05 06 07 08 09 10 11 12
 			do
-				for Prov in  "Almeri*a" #"Ca*diz" "Co*rdoba" "Granada" "Huelva" "Jae*n" "Ma*laga" "Sevilla" "Huesca" "Teruel" "Zaragoza" "Asturias" "Balear" "Palmas" "Tenerife" "Cantabria" "A*vila" "Burgos" "Leó*n" "Palencia" "Salamanca" "Segovia" "Soria" "Valladolid" "Zamora" "Albacete"  "Real" "Cuenca" "Guadalajara" "Toledo" "Barcelona" "Girona" "Lleida" "Tarragona" "Alicante" "Castello*n" "Valencia" "Badajoz" "Ca*ceres" "Coruñ*a" #"Lugo" "O*rense" "Pontevedra" "Madrid" "Navarra" "Murcia" "A[rl]a[bv]a" "[VB]iz[ck]a[iy]a" "G*ipuz*"  "Rioja"  "Ceuta" "Melilla"
+				for Prov in  "Almer[ií]a" "C[aá]diz" "C[oó]rdoba" "Granada" "Huelva" "Ja[eé]n" "M[áa]laga" "Sevilla" "Huesca" "Teruel" "Zaragoza" "Asturias" "Balear" "Palmas" "Tenerife" "Cantabria" "[AÁ]vila" "Burgos" "Le[oó]n" "Palencia" "Salamanca" "Segovia" "Soria" "Valladolid" "Zamora" "Albacete"  "Real" "Cuenca" "Guadalajara" "Toledo" "Barcelona" "Girona" "Lleida" "Tarragona" "Alicante" "Castell[oó]n" "Valencia" "Badajoz" "C[aá]ceres" "Coru*a" "Lugo" "O*rense" "Pontevedra" "Madrid" "Navarra" "Murcia" "A[rl]a[bv]a" "[VB]iz[ck]a[iy]a" "G*ipuz*"  "Rioja"  "Ceuta" "Melilla"
 				
 					do 
-						echo "$Prov" "$year" "$month"
-						Linea=$(grep -i   "$Prov"  20"$year"/T_127P_$month*.txt)
 						
-						grep -i   "$Prov"  20"$year"/T_127P_$month*.txt| awk -F'  '+ '{print $2}'|awk -F. '{print $1$2$3}' | sed "1 i\   
-						20$year$month   " >> COMBUSTIBLENUCLEAR.txt
+						File=$(find . -type d|grep -i -w */*_*$Prov*)
 
-						# $Linea| awk -F'  '+ '{print $3 > "CARBONES.txt"}'
-						# $Linea| awk -F'  '+ '{print $4 > "LIGNITOS.txt"}'
-						# $Linea| awk -F'  '+ '{print $5 > "FUEL.txt"}'
-						# $Linea| awk -F'  '+ '{print $6 > "GASNATURAL.txt"}'
-						# $Linea| awk -F'  '+ '{print $7 > "OTROSCOMBUSTIBLES.txt"}'
-						# $Linea| awk -F'  '+ '{print $8 > "TOTAL.txt"}'
+						grep -i   "$Prov"  20"$year"/T_127P_$month*.txt | awk -F'  '+ '{print $2}'|awk -F. '{print $1$2$3}' | sed "1 i\   
+						20$year$month   " >> $File/COMBUSTIBLENUCLEAR.txt
 
+						grep -i   "$Prov"  20"$year"/T_127P_$month*.txt | awk -F'  '+ '{print $3 }'|awk -F. '{print $1$2$3}' | sed "1 i\   
+						20$year$month   " >> $File/CARBONES.txt
 
+						grep -i   "$Prov"  20"$year"/T_127P_$month*.txt | awk -F'  '+ '{print $4 }'|awk -F. '{print $1$2$3}' | sed "1 i\   
+						20$year$month   " >> $File/LIGNITOS.txt
 
+						grep -i   "$Prov"  20"$year"/T_127P_$month*.txt | awk -F'  '+ '{print $5 }'|awk -F. '{print $1$2$3}' | sed "1 i\   
+						20$year$month   " >> $File/FUEL.txt
+
+						grep -i   "$Prov"  20"$year"/T_127P_$month*.txt | awk -F'  '+ '{print $6 }'|awk -F. '{print $1$2$3}' | sed "1 i\   
+						20$year$month   " >> $File/GASNATURAL.txt
 						
-
+						grep -i   "$Prov"  20"$year"/T_127P_$month*.txt | awk -F'  '+ '{print $7 }'|awk -F. '{print $1$2$3}' | sed "1 i\   
+						20$year$month   " >> $File/OTROSCOMBUSTIBLES.txt
 						
+						grep -i   "$Prov"  20"$year"/T_127P_$month*.txt | awk -F'  '+ '{print $8 }'|awk -F. '{print $1$2$3}' | sed "1 i\   
+						20$year$month   " >> $File/TOTAL.txt
 
-
-						#find . -type d|grep -i -w */*_*$Prov*
 
 					done 
 				
