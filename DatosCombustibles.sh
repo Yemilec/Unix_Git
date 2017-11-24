7bd789c2d9c422bd5559a195845bf8e1f2cb21d0
Yemile Chávez Martínez <yemi@MacBook-Pro-de-Yemile.local>
2017-11-24 11:26:39 +0100

    correccion de texto: Coruña

--- DatosCombustibles.sh
+++ DatosCombustibles.sh
@@ -4,6 +4,8 @@ for year in 06 07 08 09 10 11 12 13 14 15
 	do 
 		for month in 01 02 03 04 05 06 07 08 09 10 11 12
 			do
+				text=$(find 20"$year" -name  '*.txt'|grep -i T_127P_$month)
+				echo "`iconv -c 20"$year"/T_127P_$month*.txt  `" > $text
 				for Prov in  "Almer[ií]a" "C[aá]diz" "C[oó]rdoba" "Granada" "Huelva" "Ja[eé]n" "M[áa]laga" "Sevilla" "Huesca" "Teruel" "Zaragoza" "Asturias" "Balear" "Palmas" "Tenerife" "Cantabria" "[AÁ]vila" "Burgos" "Le[oó]n" "Palencia" "Salamanca" "Segovia" "Soria" "Valladolid" "Zamora" "Albacete"  "Real" "Cuenca" "Guadalajara" "Toledo" "Barcelona" "Girona" "Lleida" "Tarragona" "Alicante" "Castell[oó]n" "Valencia" "Badajoz" "C[aá]ceres" "Coru*a" "Lugo" "O*rense" "Pontevedra" "Madrid" "Navarra" "Murcia" "A[rl]a[bv]a" "[VB]iz[ck]a[iy]a" "G*ipuz*"  "Rioja"  "Ceuta" "Melilla"
 				
 					do 
@@ -15,31 +17,6 @@ for year in 06 07 08 09 10 11 12 13 14 15
 							do
 								Comb1=$( echo "$Comb" |awk -F: '{print $1}')
 								Comb2=$( echo "$Comb" |awk -F: '{print $2}')
-
-								
-
-								# if [ "$Comb" -eq 2 ]
-								# 	then
-								# 		NomComb=COMBUSTIBLENUCLEAR
-								# elif [ "$Comb" -eq 3 ]
-								# 	then
-								# 		NomComb=CARBONES
-								# elif [ "$Comb" -eq 4 ]
-								# 	then
-								# 		NomComb=CLIGNITOS
-								# elif [ "$Comb" -eq 5 ]
-								# 	then
-								# 		NomComb=FUEL
-								# elif [ "$Comb" -eq 6 ]
-								# 	then
-								# 		NomComb=GASNATURAL
-								# elif [ "$Comb" -eq 7 ]
-								# 	then
-								# 		NomComb=OTROSCOMBUSTIBLES
-								# elif [ "$Comb" -eq 8 ]
-								# 	then
-								# 		NomComb=TOTAL
-								# fi
 								
 								grep -i   "$Prov"  20"$year"/T_127P_$month*.txt | awk -v i=$Comb1 -F'  '+ '{print $i}' | awk  '{print $1}'|awk -v y=$year -v m=$month -F. '{print 20 y m, $1$2$3}' >> $File/"$Comb2".txt	
 
